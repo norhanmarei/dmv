c8c77ab60a267055016cd0e5186cb8a208b2f312
Norhan Marei <mustafanour751@gmail.com>
2025-11-02 04:29:47 +0300

    feat: update person entity

--- backend/DMV.Domain/Person.cs
+++ backend/DMV.Domain/Person.cs
@@ -4,14 +4,14 @@ namespace DMV.Domain;
 
 public class Person
 {
-    public int Id { set; get; }
-    public string NationalNo { set; get; } = string.Empty;
-    public string firstName { set; get; } = string.Empty;
-    public string secondName { set; get; } = string.Empty;
-    public string thirdName { set; get; } = string.Empty;
-    public string lastName { set; get; } = string.Empty;   
+    public int id { set; get; }
+    public string national_no { set; get; } = string.Empty;
+    public string first_name { set; get; } = string.Empty;
+    public string second_name { set; get; } = string.Empty;
+    public string third_name { set; get; } = string.Empty;
+    public string last_name { set; get; } = string.Empty;   
     public char gender{ set; get; }
-    public DateTime dateOfBirth{ set; get; }
+    public DateTime date_of_birth{ set; get; }
     public string phone { set; get; } = string.Empty;
     public string email { set; get; } = string.Empty;
     

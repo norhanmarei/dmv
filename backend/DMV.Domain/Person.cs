55711fa2d4ba0a6fa77c6c3e989186e819b5158d
Norhan Marei <mustafanour751@gmail.com>
2025-11-02 03:41:42 +0300

    feat: add person entity and properties

---
+++ backend/DMV.Domain/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace DMV.Domain;
+
+public class Person
+{
+    public int Id { set; get; }
+    public string NationalNo { set; get; } = string.Empty;
+    public string firstName { set; get; } = string.Empty;
+    public string secondName { set; get; } = string.Empty;
+    public string thirdName { set; get; } = string.Empty;
+    public string lastName { set; get; } = string.Empty;   
+    public char gender{ set; get; }
+    public DateTime dateOfBirth{ set; get; }
+    public string phone { set; get; } = string.Empty;
+    public string email { set; get; } = string.Empty;
+    
+}
